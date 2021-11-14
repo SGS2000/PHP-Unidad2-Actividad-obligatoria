f2494d2e9243e3fddda1f3fb79c35cf855d131e5
Santi <santiagoesquel@gmail.com>
2021-11-14 13:21:00 -0300

    Subiendo primer versión

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//Variables
+$nombre = "Santiago";
+$apellido = "García Sánchez";
+$edad = 21;
+$hobbie = "Lectura";
+$editor = "Visual Studio Code";
+$sistema = "Windows 8.1";
+
+//Imprimir
+echo "Nombre: " .$nombre;
+echo "<br>";
+echo "Apellido: " .$apellido;
+echo "<br>";
+echo "Edad: " .$edad;
+echo "<br>";
+echo "Hobbie: " .$hobbie;
+echo "<br>";
+echo "Editor preferido: " .$editor;
+echo "<br>";
+echo "Sistema operativo utilizado: " .$sistema;
+?>
